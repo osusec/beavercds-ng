b97ba12dddb95e25a582be344a20276d8b3bc3f6
Robert Detjens <github@detjens.dev>
2024-04-22 22:28:57 -0700

    add command dispatch, reorganize under src/lib
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-// I think we need this?
 use clap::Parser;
 
 mod commands;
+mod lib {
+    pub mod build;
+    pub mod check_access;
+    pub mod deploy;
+    pub mod validate;
+}
 
 fn main() {
-    let _cli = commands::Cli::parse();
+    let cli = commands::Cli::parse();
+
+    println!("args: {:?}", cli);
+
+    // dispatch commands
+    match &cli.command {
+        commands::Commands::Validate {} => lib::validate::run(),
+
+        commands::Commands::CheckAccess {
+            kubernetes,
+            frontend,
+            registry,
+        } => lib::check_access::run(kubernetes, frontend, registry),
+
+        commands::Commands::Build { profile, push } => lib::build::run(profile, push),
+
+        commands::Commands::Deploy {
+            profile,
+            no_build,
+            dry_run,
+        } => lib::deploy::run(profile, no_build, dry_run),
+    }
 }
