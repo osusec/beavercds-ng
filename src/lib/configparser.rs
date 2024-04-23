4b38ede2c7eca5eca8ef59ac323a468e2922b63e
OB <obarroncs@gmail.com>
2024-04-22 22:12:30 -0700

    Parse challenge.yml and return it. Handle errors

--- src/lib/configparser.rs
+++ src/lib/configparser.rs
@@ -1,7 +1,17 @@
 use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::collections::BTreeMap;
-use std::fs;
+use std::{fs, io};
+
+// Structs for rcds.yaml parsing
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct RCDSConfig {
+    flag_regex: String,
+    registry: Registry,
+    defaults: Defaults,
+    profiles: BTreeMap<String, ProfileConfig>,
+    points: Vec<ChallengePoints>,
+}
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -48,7 +58,7 @@ struct Defaults {
 struct ProfileConfig {
 	// deployed_challenges: BTreeMap<String, bool>,
 	frontend_url: String,
-	frontend_token: String,
+	frontend_token: Option<String>,
 	challenges_domain: String,
 	kubeconfig: String,
 	kubecontext: String,
@@ -61,20 +71,144 @@ struct ChallengePoints {
     max: i64,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct RCDSConfig {
-    flag_regex: String,
-    registry: Registry,
-    defaults: Defaults,
-    profiles: BTreeMap<String, ProfileConfig>,
-    points: Vec<ChallengePoints>,
+#[derive(Debug)]
+pub enum ParseError {
+    YAMLParseError(serde_yaml::Error),
+    IOError(std::io::Error),
+}
+
+impl From<io::Error> for ParseError {
+    fn from(err: io::Error) -> Self {
+        ParseError::IOError(err)
+    }
+}
+
+impl From<serde_yaml::Error> for ParseError {
+    fn from(err: serde_yaml::Error) -> Self {
+        ParseError::YAMLParseError(err)
+    }
+}
+
+pub fn parse_rcds_config() -> Result<RCDSConfig,ParseError> {
+    let my_stuff = fs::read_to_string("rcds.yaml")?;
+
+    let application_data: RCDSConfig = serde_yaml::from_str(&my_stuff)?;
+    println!("{:?}", application_data);
+    return Ok(application_data);
 }
 
 
-fn parse_rcds_config() {
-    let my_stuff = fs::read_to_string("rcds.yaml")
-        .expect("Should have been able to read the file");
+// Structs for challenge.yaml parsing
 
-    let application_data: RCDSConfig = serde_yaml::from_str(&my_stuff).unwrap();
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct ChallengeConfig {
+    name: String,
+    author: String,
+    description: String,
+    difficulty: i64,
+    flag: FlagType,
+    provide: Vec<String>,
+    pods: Vec<Pod>
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum FlagType {
+    RawString(String),
+    File(FilePath),
+    Text(FileText),
+    Regex(FileRegex),
+    Verifier(FileVerifier),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct FilePath {
+    file: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct FileText {
+    text: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct FileRegex {
+    regex: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct FileVerifier {
+    verifier: String,
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Pod {
+    name: String,
+    build: BuildSpec,
+    image: String,
+    env: Option<ListOrMap>,
+    resources: Option<Resource>,
+    replicas: i64,
+    ports: Vec<PortConfig>,
+    volume: Option<String>,
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum BuildSpec {
+    CONTEXT(String),
+    MAP(BTreeMap<String, String>),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct BuildObject {
+    context: String,
+    dockerfile: String,
+    dockerfile_inline: String,
+    args: ListOrMap,
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum ListOrMap {
+    LIST(Vec<String>),
+    MAP(BTreeMap<String, String>),
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct PortConfig {
+    internal: i64,
+    expose: PortType
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum PortType {
+    TCP(TCPPort),
+    HTTP(HTTPEndpoint),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct TCPPort {
+    tcp: i64,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct HTTPEndpoint {
+    http: String,
+}
+
+
+
+
+pub fn parse_challenge_config(path: &str) -> Result<ChallengeConfig,ParseError> {
+    let my_stuff = fs::read_to_string(path)?;
+
+    let application_data: ChallengeConfig = serde_yaml::from_str(&my_stuff)?;
     println!("{:?}", application_data);
+    return Ok(application_data);
 }
