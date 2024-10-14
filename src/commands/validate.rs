68a9115c349c915f3b9cea2c4a6025c69553b462
Robert Detjens <github@detjens.dev>
2024-10-13 18:20:14 -0700

    validate challenges listed in profile deploy settings exist
    also adds a directory field to ChallengeConfig struct to hold the reference
    path for the challenge
    
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/commands/validate.rs
+++ src/commands/validate.rs
@@ -1,25 +1,62 @@
-use crate::configparser::{get_challenges, get_config};
 use simplelog::*;
+use std::path::Path;
 use std::process::exit;
+
+use crate::configparser::{get_challenges, get_config, get_profile_deploy};
 
 pub fn run() {
     info!("validating config...");
-    match get_config() {
-        Ok(_) => info!("  config ok!"),
+    let config = match get_config() {
+        Ok(c) => c,
         Err(err) => {
             error!("{err:#}");
             exit(1);
         }
-    }
+    };
+    info!("  config ok!");
 
     info!("validating challenges...");
-    match get_challenges() {
-        Ok(_) => info!("  challenges ok!"),
+    let chals = match get_challenges() {
+        Ok(c) => c,
         Err(errors) => {
             for e in errors.iter() {
                 error!("{e:#}");
             }
             exit(1);
         }
+    };
+    info!("  challenges ok!");
+
+    // check global deploy settings for invalid challenges
+    info!("validating deploy config...");
+    for (profile_name, _pconfig) in config.profiles.iter() {
+        // get em
+        let deploy_challenges = match get_profile_deploy(profile_name) {
+            Ok(d) => &d.challenges,
+            Err(err) => {
+                error!("{err:#}");
+                exit(1);
+            }
+        };
+
+        // check em
+        let missing: Vec<_> = deploy_challenges
+            .keys()
+            .filter_map(
+                // invert match to filter for challenges that *dont* match
+                |path| match chals.iter().find(|c| c.directory == Path::new(path)) {
+                    Some(_) => None,
+                    None => Some(path),
+                },
+            )
+            .collect();
+        if missing.len() > 0 {
+            error!(
+                "Deploy settings for profile '{profile_name}' has challenges that do not exist:"
+            );
+            missing.iter().for_each(|path| error!("  - {path}"));
+            exit(1)
+        }
     }
+    info!("  deploy ok!")
 }
