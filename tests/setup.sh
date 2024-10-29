198905c3e92f6f67ff6ec0400054511e989f1019
Robert Detjens <github@detjens.dev>
2024-10-28 20:56:05 -0700

    Add test infra script and compose file
    Spins up a local minikube cluster and registry and S3/minio containers.
    
    Signed-off-by: Robert Detjens <github@detjens.dev>

---
+++ tests/setup.sh
@@ -0,0 +1,51 @@
+# #!/bin/bash
+
+exit_cmd (){
+  [ "$BASH_SOURCE" = "$0" ] && echo exit || echo return
+}
+
+dockpod (){
+  command -v podman || echo docker
+}
+
+export MINIKUBE_PROFILE=beavercds
+COMPOSE_FILE="$(git rev-parse --show-toplevel)/tests/services.compose.yaml"
+
+start_stuff (){
+  # start cluster
+  minikube start --container-runtime=cri-o
+
+  # start registry
+  $(dockpod) compose -f $COMPOSE_FILE up -d
+
+  # export variables if sourced or echo them if run
+  export BEAVERCDS_REGISTRY_DOMAIN="host.minikube.internal:5000/testing"
+  export BEAVERCDS_PROFILES_TESTING_KUBECONTEXT="$MINIKUBE_PROFILE"
+  export BEAVERCDS_PROFILES_TESTING_S3_ENDPOINT="localhost:9000"
+  export BEAVERCDS_PROFILES_TESTING_S3_REGION=""
+  export BEAVERCDS_PROFILES_TESTING_S3_ACCESSKEY=$(cat $COMPOSE_FILE | yq -r .services.minio.environment.MINIO_ROOT_USER)
+  export BEAVERCDS_PROFILES_TESTING_S3_SECRETKEY=$(cat $COMPOSE_FILE | yq -r .services.minio.environment.MINIO_ROOT_PASSWORD)
+
+  if [ $(exit_cmd) = "exit" ] ; then
+    echo
+    echo "export these vars manually, or source this script to export"
+    env | grep BEAVERCDS | sort
+  fi
+}
+
+stop_stuff (){
+  minikube delete
+  $(dockpod) compose -f $(git rev-parse --show-toplevel)/tests/services.compose.yaml down --volumes
+}
+
+
+case "${1:-}" in
+  start | up) start_stuff ;;
+  stop | down | rm) stop_stuff ;;
+  *)
+    echo "usage:" 1>&2
+    echo "  $0 up" 1>&2
+    echo "  $0 down" 1>&2
+    $(exit_cmd) 2
+  ;;
+esac
