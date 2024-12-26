0a23a9d6f443ea79d5a0c38fe32275110064dbb1
Robert Detjens <github@detjens.dev>
2024-12-26 15:00:28 -0800

    WIP build artifacts copy, getting empty dl tar
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/builder/docker.rs
+++ src/builder/docker.rs
@@ -1,14 +1,21 @@
 use anyhow::{anyhow, bail, Context, Error, Result};
 use bollard::auth::DockerCredentials;
+use bollard::container::{
+    Config, CreateContainerOptions, DownloadFromContainerOptions, RemoveContainerOptions,
+};
 use bollard::errors::Error as DockerError;
 use bollard::image::{BuildImageOptions, PushImageOptions};
 use bollard::Docker;
 use core::fmt;
-use futures_util::{StreamExt, TryStreamExt};
+use futures::{StreamExt, TryStreamExt};
 use simplelog::*;
+use std::fs::File;
+use std::io::{Seek, Write};
+use std::sync::LazyLock;
+use std::{fs, io};
 use std::{io::Read, path::Path};
 use tar;
-use tempfile::tempfile;
+use tempfile::{tempdir_in, tempfile};
 use tokio;
 
 use crate::configparser::challenge::BuildObject;
@@ -17,10 +24,7 @@ use crate::configparser::UserPass;
 #[tokio::main(flavor = "current_thread")] // make this a sync function
 pub async fn build_image(context: &Path, options: &BuildObject, tag: &str) -> Result<String> {
     trace!("building image in directory {context:?} to tag {tag:?}");
-    let client = client()
-        .await
-        // truncate error chain with new error (returned error is way too verbose)
-        .map_err(|_| anyhow!("could not talk to Docker daemon (is DOCKER_HOST correct?)"))?;
+    let client = client().await?;
 
     let build_opts = BuildImageOptions {
         dockerfile: options.dockerfile.clone(),
@@ -76,10 +80,7 @@ pub async fn build_image(context: &Path, options: &BuildObject, tag: &str) -> Re
 #[tokio::main(flavor = "current_thread")] // make this a sync function
 pub async fn push_image(image_tag: &str, creds: &UserPass) -> Result<String> {
     info!("pushing image {image_tag:?} to registry");
-    let client = client()
-        .await
-        // truncate error chain with new error (returned error is way too verbose)
-        .map_err(|_| anyhow!("could not talk to Docker daemon (is DOCKER_HOST correct?)"))?;
+    let client = client().await?;
 
     let (image, tag) = image_tag
         .rsplit_once(":")
@@ -114,15 +115,104 @@ pub async fn push_image(image_tag: &str, creds: &UserPass) -> Result<String> {
     Ok(tag.to_string())
 }
 
+#[tokio::main(flavor = "current_thread")] // make this a sync function
+pub async fn create_container(image_tag: &str, name: &str) -> Result<String> {
+    debug!("creating container {name:?} from image {image_tag:?}");
+    let client = client().await?;
+
+    let opts = CreateContainerOptions {
+        name: name.to_string(),
+        ..Default::default()
+    };
+    let config = Config {
+        image: Some(image_tag),
+        ..Default::default()
+    };
+
+    let container = client.create_container(Some(opts), config).await?;
+    Ok(container.id)
+}
+
+#[tokio::main(flavor = "current_thread")] // make this a sync function
+pub async fn remove_container(name: &str) -> Result<()> {
+    debug!("removing container {name:?}");
+    let client = client().await?;
+
+    let opts = RemoveContainerOptions {
+        force: true,
+        ..Default::default()
+    };
+    client.remove_container(name, Some(opts)).await?;
+
+    Ok(())
+}
+
+pub async fn copy_file(
+    container_id: &str,
+    from_path: &str,
+    rename_to: Option<&str>,
+) -> Result<String> {
+    let client = client().await?;
+
+    // if no rename is given, use basename of `from` as target path
+    let target_path = match rename_to {
+        Some(to) => to,
+        None => Path::new(from_path).file_name().unwrap().to_str().unwrap(),
+    };
+
+    info!("copying {container_id}:{from_path} to {target_path}");
+
+    // Download single file from container in an archive
+    let opts = DownloadFromContainerOptions { path: from_path };
+    let mut dl_stream = client.download_from_container(container_id, Some(opts));
+
+    // scratch dir in chal repo (two vars for scoping reasons)
+    // let mut tempdir_full = tempdir_in(".")?;
+    // let tempdir = tempdir_full.path();
+
+    fs::create_dir("./.tempdir");
+    let tempdir = Path::new("./.tempdir");
+
+    // collect byte stream chunks into full file
+    let mut tarfile = File::create(tempdir.join(format!("download_{target_path}.tar")))?;
+    while let Some(chunk) = dl_stream.next().await {
+        tarfile.write_all(&chunk?)?;
+    }
+    tarfile.rewind();
+
+    // unpack file retrieved to temp dir
+    trace!("extracting download tar to {:?}", tempdir);
+    let mut tar = tar::Archive::new(tarfile);
+
+    // extract single file from archive to disk
+    // we only copied out one file, so this tar should only have one file
+    if let Some(Ok(mut entry)) = tar.entries()?.next() {
+        let mut target = File::create_new(target_path)?;
+        io::copy(&mut entry, &mut target);
+    } else {
+        bail!("downloaded archive for {container_id}:{from_path} has no files in it!");
+    }
+
+    Ok(target_path.to_string())
+}
+
 //
 // helper functions
 //
-pub async fn client() -> Result<Docker> {
-    debug!("connecting to docker...");
-    let client = Docker::connect_with_defaults()?;
-    client.ping().await?;
 
-    Ok(client)
+// connect to Docker/Podman daemon once and share client
+static CLIENT: LazyLock<std::result::Result<Docker, bollard::errors::Error>> =
+    LazyLock::new(|| {
+        debug!("connecting to docker...");
+        Docker::connect_with_defaults()
+    });
+pub async fn client() -> Result<Docker> {
+    let c = CLIENT
+        .as_ref()
+        .map_err(|_| anyhow!("could not talk to Docker daemon (is DOCKER_HOST correct?)"))?;
+    c.ping().await?;
+
+    Ok(c.clone())
 }
 
 #[derive(Debug)]
--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -3,7 +3,7 @@
 
 use anyhow::{anyhow, Context, Error, Result};
 use bollard::image::BuildImageOptions;
-use futures_util::stream::Iter;
+use futures::stream::Iter;
 use itertools::Itertools;
 use simplelog::*;
 use std::default;
@@ -11,50 +11,54 @@ use std::fmt::Pointer;
 use std::iter::zip;
 use std::path::Path;
 
-use crate::configparser::challenge::{BuildObject, ChallengeConfig, ImageSource::*};
-use crate::configparser::{get_challenges, get_config, get_profile_config, get_profile_deploy};
+use crate::configparser::challenge::{
+    BuildObject, ChallengeConfig, ImageSource::*, Pod, ProvideConfig,
+};
+use crate::configparser::{enabled_challenges, get_config};
 
 pub mod docker;
-use docker::{build_image, push_image};
+
+pub mod artifacts;
+use artifacts::extract_asset;
+
+// define tag format as reusable macro
+macro_rules! image_tag_str {
+    () => {
+        "{registry}/{challenge}-{container}:{profile}"
+    };
+}
 
 /// Build all enabled challenges for the given profile. Returns tags built
-pub fn build_challenges(profile_name: &str) -> Result<Vec<String>> {
+pub fn build_challenges(
+    profile_name: &str,
+    push: bool,
+    extract_artifacts: bool,
+) -> Result<Vec<String>> {
     enabled_challenges(profile_name)?
         .iter()
-        .map(|chal| build_challenge_images(profile_name, chal))
+        .map(|chal| build_challenge(profile_name, chal, push, extract_artifacts))
         .flatten_ok()
         .collect::<Result<_>>()
 }
 
-/// Get all enabled challenges for profile
-pub fn enabled_challenges(profile_name: &str) -> Result<Vec<&ChallengeConfig>> {
-    let config = get_config()?;
-    let challenges = get_challenges().unwrap();
-    let deploy = &get_profile_deploy(profile_name)?.challenges;
-
-    let enabled = deploy
-        .iter()
-        .filter_map(|(chal, enabled)| match enabled {
-            true => challenges.iter().find(|c| c.directory == Path::new(chal)),
-            false => None,
-        })
-        .collect();
-
-    Ok(enabled)
-}
-
-/// Build all images for challenge under given path, return image tag
-fn build_challenge_images(profile_name: &str, chal: &ChallengeConfig) -> Result<Vec<String>> {
+/// Build all images from given challenge, optionally pushing image or extracting artifacts
+fn build_challenge(
+    profile_name: &str,
+    chal: &ChallengeConfig,
+    push: bool,
+    extract_artifacts: bool,
+) -> Result<Vec<String>> {
     debug!("building images for chal {:?}", chal.directory);
     let config = get_config()?;
 
-    chal.pods
+    let built_tags: Vec<_> = chal
+        .pods
         .iter()
         .filter_map(|p| match &p.image_source {
             Image(_) => None,
             Build(b) => {
                 let tag = format!(
-                    "{registry}/{challenge}-{container}:{profile}",
+                    image_tag_str!(),
                     registry = config.registry.domain,
                     challenge = chal.name,
                     container = p.name,
@@ -71,20 +75,109 @@ fn build_challenge_images(profile_name: &str, chal: &ChallengeConfig) -> Result<
                 )
             }
         })
-        .collect::<Result<_>>()
-}
-
-/// Push passed tags to registry
-pub fn push_tags(tags: Vec<String>) -> Result<Vec<String>> {
-    let config = get_config()?;
-
-    let built_tags = tags
-        .iter()
-        .map(|tag| {
-            push_image(tag, &config.registry.build)
-                .with_context(|| format!("error pushing image {tag}"))
-        })
         .collect::<Result<_>>()?;
 
+    if push {
+        debug!(
+            "pushing {} tags for chal {:?}",
+            built_tags.len(),
+            chal.directory
+        );
+
+        built_tags
+            .iter()
+            .map(|tag| {
+                docker::push_image(tag, &config.registry.build)
+                    .with_context(|| format!("error pushing image {tag}"))
+            })
+            .collect::<Result<Vec<_>>>()?;
+    }
+
+    if extract_artifacts {
+        // find the matching tag for Provide entries that have a `from:` source
+        let image_assoc = chal
+            .provide
+            .iter()
+            .filter_map(|p| {
+                p.from.as_ref().map(|f| {
+                    (
+                        p,
+                        format!(
+                            image_tag_str!(),
+                            registry = config.registry.domain,
+                            challenge = chal.name,
+                            container = f,
+                            profile = profile_name
+                        ),
+                    )
+                })
+            })
+            .collect_vec();
+
+        debug!(
+            "extracting {} build artifacts for chal {:?}",
+            image_assoc.len(),
+            chal.directory
+        );
+
+        let assets = image_assoc
+            .into_iter()
+            .map(|(p, tag)| {
+                let name = format!(
+                    "asset-container-{}-{}",
+                    chal.directory.to_string_lossy().replace("/", "-"),
+                    p.from.clone().unwrap()
+                );
+                let container = docker::create_container(&tag, &name)?;
+
+                let asset_result = extract_asset(p, &container);
+
+                // clean up container even if it failed
+                docker::remove_container(&name)?;
+
+                asset_result
+            })
+            .flatten_ok()
+            .collect::<Result<Vec<_>>>()?;
+
+        debug!("Extracted assets: {:?}", assets);
+    }
     Ok(built_tags)
 }
+
+// /// Push passed tags to registry
+// pub fn push_tags(tags: Vec<String>) -> Result<Vec<String>> {
+//     let config = get_config()?;
+
+//     let built_tags = tags
+//         .iter()
+//         .map(|tag| {
+//             push_image(tag, &config.registry.build)
+//                 .with_context(|| format!("error pushing image {tag}"))
+//         })
+//         .collect::<Result<_>>()?;
+
+//     Ok(built_tags)
+// }
+
+// /// Extract any assets needed from given challenges
+// pub fn extract_assets(
+//     profile_name: &str,
+//     built_chals: Vec<&ChallengeConfig>,
+// ) -> Result<Vec<String>> {
+//     built_chals.iter().map(|chal| {
+//         chal.provide.iter().filter(|p| p.from.is_some()).map(|p| {
+//             assets::extract_asset(p, container)
+//         })
+
+//         // let tag = format!(
+//         //     image_tag!(),
+//         //     registry = config.registry.domain,
+//         //     challenge = chal.name,
+//         //     container = p.name,
+//         //     profile = profile_name
+//         // );
+//     });
+
+//     Ok(vec![])
+// }
