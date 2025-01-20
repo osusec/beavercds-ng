d11d7e844fc5d71fcaec05e6478b5792fa820ea0
Robert Detjens <github@detjens.dev>
2025-01-19 22:54:28 -0800

    Switch to k3d since minikube has problems with cert-manager
    for some reason, minikube running rootless cannot extract the images used
    by cert-manager (tar permission denied). k3d does not have this problem
    and is faster to set up anyway.
    
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- tests/setup.sh
+++ tests/setup.sh
@@ -13,14 +13,29 @@ COMPOSE_FILE="$(git rev-parse --show-toplevel)/tests/services.compose.yaml"
 
 start_stuff (){
   # start cluster
-  minikube start --container-runtime=cri-o
+
+  # rootless minikube has problems with extracting the cert-manager images for
+  # some reason, so switch to k3d instead
+  #
+  # minikube start --container-runtime=cri-o
+
+  # rootless podman? add kubelet-in-rootless arg
+  if $(dockpod) info --format={{.Host.Security.Rootless}} | grep -q true ; then
+    ROOTLESS_ARG='--k3s-arg=--kubelet-arg=feature-gates=KubeletInUserNamespace=true@server:*'
+  else
+    ROOTLESS_ARG=''
+  fi
+
+  k3d cluster create "$MINIKUBE_PROFILE" \
+    --k3s-arg "--disable=traefik@server:*" \
+    "$ROOTLESS_ARG"
 
   # start registry
   $(dockpod) compose -f $COMPOSE_FILE up -d
 
   # export variables if sourced or echo them if run
   export BEAVERCDS_REGISTRY_DOMAIN="localhost:5000/testing"
-  export BEAVERCDS_PROFILES_TESTING_KUBECONTEXT="$MINIKUBE_PROFILE"
+  export BEAVERCDS_PROFILES_TESTING_KUBECONTEXT="k3d-$MINIKUBE_PROFILE"
   export BEAVERCDS_PROFILES_TESTING_S3_ENDPOINT="http://localhost:9000"
   export BEAVERCDS_PROFILES_TESTING_S3_REGION=""
   export BEAVERCDS_PROFILES_TESTING_S3_ACCESS_KEY=$(cat $COMPOSE_FILE | yq -r .services.minio.environment.MINIO_ROOT_USER)
@@ -34,7 +49,8 @@ start_stuff (){
 }
 
 stop_stuff (){
-  minikube delete
+  # minikube delete
+  k3d cluster delete "$MINIKUBE_PROFILE"
   $(dockpod) compose -f $(git rev-parse --show-toplevel)/tests/services.compose.yaml down --volumes
 }
 
