2f01285fc972ddd696f549f155619fe5204a671d
Robert Detjens <github@detjens.dev>
2025-01-29 20:35:27 -0800

    some deploy command skeleton
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -7,14 +7,15 @@ use std::env::current_exe;
 use std::process::exit;
 
 use crate::clients::kube_client;
-use crate::cluster_setup as setup;
+use crate::cluster_setup;
 use crate::configparser::config::ProfileConfig;
 use crate::configparser::{get_config, get_profile_config};
 
-#[tokio::main(flavor = "current_thread")] // make this a sync function
-pub async fn run(profile_name: &str, _no_build: &bool, _dry_run: &bool) {
-    info!("deploying challenges...");
+use crate::builder::build_challenges;
+use crate::deploy::{deploy_challenges, update_frontend, upload_assets};
 
+#[tokio::main(flavor = "current_thread")] // make this a sync function
+pub async fn run(profile_name: &str, no_build: &bool, _dry_run: &bool) {
     let profile = get_profile_config(profile_name).unwrap();
 
     // has the cluster been setup?
@@ -23,13 +24,57 @@ pub async fn run(profile_name: &str, _no_build: &bool, _dry_run: &bool) {
         exit(1);
     }
 
-    // let build_result =
+    // build before deploying
+    if *no_build {
+        warn!("");
+        warn!("Not building before deploying! are you sure this is a good idea?");
+        warn!("");
+    }
+
+    info!("building challenges...");
+    let build_results = match build_challenges(profile_name, true, true) {
+        Ok(result) => result,
+        Err(e) => {
+            error!("{e:?}");
+            exit(1);
+        }
+    };
+
+    // deploy needs to:
+    // A) render kubernetes manifests
+    //    - namespace, deployment, service, ingress
+    //    - upgrade ingress config with new listen ports
+    //
+    // B) upload asset files to bucket
+    //
+    // C) update frontend with new state of challenges
+
+    // A)
+    info!("deploying challenges...");
+    if let Err(e) = deploy_challenges(profile_name, &build_results).await {
+        error!("{e:?}");
+        exit(1);
+    }
+
+    // B)
+    info!("deploying challenges...");
+    if let Err(e) = upload_assets(profile_name, &build_results).await {
+        error!("{e:?}");
+        exit(1);
+    }
+
+    // A)
+    info!("deploying challenges...");
+    if let Err(e) = update_frontend(profile_name, &build_results).await {
+        error!("{e:?}");
+        exit(1);
+    }
 }
 
 /// check to make sure that the needed ingress charts are deployed and running
 async fn check_setup(profile: &ProfileConfig) -> Result<()> {
     let kube = kube_client(profile).await?;
-    let secrets: kube::Api<Secret> = kube::Api::namespaced(kube, setup::INGRESS_NAMESPACE);
+    let secrets: kube::Api<Secret> = kube::Api::namespaced(kube, cluster_setup::INGRESS_NAMESPACE);
 
     let all_releases = secrets
         .list_metadata(&ListParams::default().labels("owner=helm"))
@@ -93,12 +138,15 @@ async fn check_setup(profile: &ProfileConfig) -> Result<()> {
             .iter()
             .fold(Err(anyhow!("")), |e, reason| match reason {
                 ChartFailure::Missing(c) => e.with_context(|| {
-                    format!("chart {}/{c} is not deployed", setup::INGRESS_NAMESPACE)
+                    format!(
+                        "chart {}/{c} is not deployed",
+                        cluster_setup::INGRESS_NAMESPACE
+                    )
                 }),
                 ChartFailure::DeploymentFailed(c) => e.with_context(|| {
                     format!(
                         "chart {}/{c} is in a failed state",
-                        setup::INGRESS_NAMESPACE
+                        cluster_setup::INGRESS_NAMESPACE
                     )
                 }),
             })
