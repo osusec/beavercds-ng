7d443181f222737eaedf933df7497b623571fe8c
Robert Detjens <github@detjens.dev>
2025-03-01 15:00:00 -0800

    Remove custom template env remnants
    These were added to test overriding the minijinja `default` None handling.
    Gating in the template was easier, and so this is not needed now.
    
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/deploy/kubernetes/templates.rs
+++ src/deploy/kubernetes/templates.rs
@@ -1,5 +1,3 @@
-use minijinja::{context, escape_formatter, Environment, Value};
-
 // Embed Kubernetes template files into binary.
 
 pub static CHALLENGE_NAMESPACE: &str =
@@ -13,33 +11,3 @@ pub static CHALLENGE_SERVICE_HTTP: &str =
 
 pub static CHALLENGE_SERVICE_TCP: &str =
     include_str!("../../asset_files/challenge_templates/tcp.yaml.j2");
-
-/// Build template environment with None as default
-/// https://github.com/mitsuhiko/minijinja/tree/main/examples/none-as-undefined
-pub fn template_env() -> Environment<'static> {
-    let mut env = Environment::new();
-
-    env.add_filter("default", none_default);
-    env.set_formatter(|out, state, value| {
-        escape_formatter(
-            out,
-            state,
-            if value.is_none() {
-                &Value::UNDEFINED
-            } else {
-                value
-            },
-        )
-    });
-
-    env
-}
-
-/// Similar to the regular `default` filter but also handles `none`.
-pub fn none_default(value: Value, other: Option<Value>) -> Value {
-    if value.is_undefined() || value.is_none() {
-        other.unwrap_or_else(|| Value::from(""))
-    } else {
-        value
-    }
-}
