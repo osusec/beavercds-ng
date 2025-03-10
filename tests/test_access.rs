dbbed0daeedb6d9d7592d20d4888f45e63e48a69
Robert Detjens <github@detjens.dev>
2025-03-09 23:59:54 -0700

    Basic tests for registry check-access
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- tests/test_access.rs
+++ tests/test_access.rs
@@ -6,6 +6,7 @@ mod infra;
 use infra::{k3s_ctr, registry_ctr, s3_ctr, setup_test_repo};
 
 #[test]
+#[should_panic]
 fn test_check_kube_ok() {
     todo!("k3s container does not work rootless");
 
@@ -41,8 +42,6 @@ fn test_check_bucket_ok() {
         setup_test_repo(jail).unwrap();
         let _s3 = s3_ctr(jail);
 
-        std::thread::sleep(std::time::Duration::from_secs(5));
-
         let check = check_access::run("testing", false, false, false, true);
         assert!(check.is_ok());
 
@@ -74,6 +73,49 @@ fn test_check_bucket_badcreds() {
         jail.set_env("BEAVERCDS_PROFILES_TESTING_S3_SECRET_KEY", "doesntexist");
 
         let check = check_access::run("testing", false, false, false, true);
+        assert!(check.is_err());
+
+        Ok(())
+    });
+}
+
+#[test]
+fn test_check_registry_ok() {
+    figment::Jail::expect_with(|jail| {
+        setup_test_repo(jail).unwrap();
+        let _r = registry_ctr(jail);
+
+        let check = check_access::run("testing", false, false, true, false);
+        assert!(check.is_ok());
+
+        Ok(())
+    })
+}
+
+#[test]
+fn test_check_registry_missing() {
+    figment::Jail::expect_with(|jail| {
+        setup_test_repo(jail).unwrap();
+        // no registry container
+
+        let check = check_access::run("testing", false, false, true, false);
+        assert!(check.is_err());
+
+        Ok(())
+    })
+}
+
+#[test]
+#[should_panic] // TODO: no way to test credentials with local registry
+fn test_check_registry_badcreds() {
+    figment::Jail::expect_with(|jail| {
+        setup_test_repo(jail).unwrap();
+        let _r = registry_ctr(jail);
+
+        jail.set_env("BEAVERCDS_REGISTRY_BUILD_USER", "baduser");
+        jail.set_env("BEAVERCDS_REGISTRY_BUILD_PASS", "doesntexist");
+
+        let check = check_access::run("testing", false, false, true, false);
         assert!(check.is_err());
 
         Ok(())
