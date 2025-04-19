6cf397edcd536c7240c0c19fe323f6f16c58769d
Phoenix Angulo <26429095+Kajmany@users.noreply.github.com>
2025-04-18 22:35:22 -0700

    Get a lot more info (nanoseconds, span in/out, thread id) at -vv+ verbosity levels

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
 use beavercds_ng::commands;
 use clap::Parser;
 use tracing::{trace, Level};
-use tracing_subscriber::{fmt::time, EnvFilter};
+use tracing_subscriber::{
+    fmt::{format::FmtSpan, time},
+    EnvFilter,
+};
 
 mod cli;
 
 fn main() {
     let cli = cli::Cli::parse();
 
-    // number of 'v' flags influences our crate's log level, all other log levels, and whether or
-    // not we display the span stack, respectively
-    let (brcds_level, dep_level, display_spans) = match cli.verbosity {
-        0 => (Level::INFO, Level::WARN, false),
-        1 => (Level::DEBUG, Level::INFO, false),
-        2 => (Level::TRACE, Level::DEBUG, true),
-        _ => (Level::TRACE, Level::TRACE, true),
+    // number of 'v' flags influences our crate's log level and all other log levels separately
+    let (brcds_level, dep_level) = match cli.verbosity {
+        0 => (Level::INFO, Level::WARN),
+        1 => (Level::DEBUG, Level::INFO),
+        2 => (Level::TRACE, Level::DEBUG),
+        _ => (Level::TRACE, Level::TRACE),
+    };
+    // this is our threshold for a lot more (but far from all) information
+    let (extra_toggles, timestr, events) = if cli.verbosity >= 2 {
+        (true, "%H:%M:%S%.f", FmtSpan::NEW | FmtSpan::CLOSE)
+    } else {
+        (false, "%H:%M:%S", FmtSpan::NONE)
     };
 
     // Use RUST_LOG env variable to set log levels if it's set
     // Otherwise we use the above levels. Span-stack display always influenced by -v
-    let timer = time::ChronoLocal::new("%H:%M:%S".to_owned());
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
             format!("{}={brcds_level},{dep_level}", env!("CARGO_CRATE_NAME")).into()
         }))
-        .with_timer(timer)
-        .with_target(display_spans)
+        .with_timer(time::ChronoLocal::new(timestr.to_string()))
+        .with_target(extra_toggles)
+        .with_thread_ids(extra_toggles)
+        .with_span_events(events)
         .init();
 
     trace!("args: {:?}", cli);
-
     // dispatch commands
     match &cli.command {
         cli::Commands::Validate => commands::validate::run(),
